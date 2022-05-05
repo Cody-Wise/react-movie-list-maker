1a758b03110d39764d7c863849a62d23b736fa93
Cody-Wise <cody@sharptackmedia.com>
2022-05-05 12:54:16 -0700

    Finished Movie List

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import Movie from './Movie';
 
-export default function MovieList() {
-  return <div>MovieList</div>;
+export default function MovieList(props) {
+  return (
+    <div className="list">
+      {props.allMovies.map((movie, i) => (
+        <Movie
+          key={`${props.movieFormTitle}-${props.movieFormYearReleased}-${i}`}
+          title={movie.movieFormTitle}
+          year={movie.movieFormYearReleased}
+          director={movie.movieFormDirector}
+          posterColor={movie.movieFormColor}
+          handleDeleteMovie={props.handleDeleteMovie}
+        />
+      ))}
+    </div>
+  );
 }
